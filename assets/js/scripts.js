0dfda5cb2ba99cf6f51772aae27bc3f66e4714b7
Alex <alexander.h.perry@gmail.com>
2022-07-10 20:24:01 +1000

    Geolocation function, Weather function, current and forecast weather html updates, button onclick function - todo: localstorage

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,11 +5,7 @@
 // fetch to pull data
 // sort data by current and 5 day forecast
 //
-// current
-// city, current date - Icon
-// temp. wind, humidity, uv index
-// forecast
-// date, icon, temp, wind, humidity
+
 
 // getGeoCode function
 // args: city
@@ -22,7 +18,10 @@ function getGeoCode(city) {
                 response.json().then(function (data) {
                     let lat = data[0].lat;
                     let lon = data[0].lon;
-                    console.log("from fetch", lat);
+                    // Run getWeather function
+                    console.log("geocode")
+                    console.log(data);
+                    $(current_city).html(data[0].name);
                     getWeather(lat, lon);
                 });
             } else {
@@ -34,10 +33,57 @@ function getGeoCode(city) {
 
 // getWeather function
 // args: lat, lon
-// returns something
+// Updates Current weather HTML, and 5 day average. 
+// Forecast: index[0] is current day
 function getWeather(lat, lon) {
-    const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,alerts&units=metric&appid=1b06c75812b8caa0f23e09c99756195a";
+    const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=metric&appid=1b06c75812b8caa0f23e09c99756195a";
     console.log('getweather', lat, lon);
+    fetch(apiURL)
+        .then(response => {
+            if (response.ok) {
+                response.json().then(data => {
+                    // Current Weather
+                    $(current_city).append('<img src="http://openweathermap.org/img/wn/' + data.current.weather[0].icon + '.png" alt="current weather icon" />')
+                    $(current_temp).html('Temp: ' + data.current.temp + "°C");
+                    $(current_wind).html('Wind: ' + data.current.wind_speed + 'kph');
+                    $(current_humidity).html('Humidity: ' + data.current.humidity + '%');
+                    $(current_UV).html('UV Index: '+ data.current.uvi);
+                    console.log("weatherdata");
+                    console.log(data);
+
+                    // 5 Day forecast
+                    // date, icon, temp, wind, humidity, 
+                    $(".day").each(function (index) {
+                        console.log(index);
+                        console.log(data.daily[index + 1].dt);
+                        let icon = data.daily[index + 1].weather[0].icon;
+                        $(this).html(
+                            "<ul><li><h4>" + moment.unix(data.daily[index + 1].dt).format("DD/MM/YYYY") + "</h4></li>" +
+                            '<li><img src="http://openweathermap.org/img/wn/' + icon + '.png" alt = "weather icon" /></li>' +
+                            "<li>Temp: " + data.daily[index + 1].temp.day + "°C</li>" +
+                            "<li>Wind: " + data.daily[index + 1].wind_speed + "kph</li>" +
+                            "<li>Humidity: " + data.daily[index + 1].humidity + "%</li></ul>"
+                        );
+                    })
+                })
+            } else alert("Error getting weather data");
+    })
+};
+
+//Search button handler.
+$("#btnSearch").on("click", function () {
+    const city = $("#cityInput").val();
+    console.log(city);
+    //save to local storage
+    saveLocal(city);
+    // run geocode function => weather function
+    getGeoCode(city);
+});
+
+function saveLocal() {
+    //funtion data here
 }
 
-getGeoCode("sydney")
+function loadLocal() {
+ //funtion data here
+}
