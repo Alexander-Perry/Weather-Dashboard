85c7db656ec5c2ad7d095bd2680da6f19c7240e3
Alex <alexander.h.perry@gmail.com>
2022-07-10 23:16:37 +1000

    Localstorage functions added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,7 @@
 
 
-// Button triggers
-// geolocation of city entered
-// fetch to pull data
-// sort data by current and 5 day forecast
-//
-
-
 // getGeoCode function
-// args: city
+// args: city - from inputbox
 // runs getWeather function on success
 function getGeoCode(city) {
     const apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=1b06c75812b8caa0f23e09c99756195a";
@@ -47,7 +40,7 @@ function getWeather(lat, lon) {
                     $(current_temp).html('Temp: ' + data.current.temp + "°C");
                     $(current_wind).html('Wind: ' + data.current.wind_speed + 'kph');
                     $(current_humidity).html('Humidity: ' + data.current.humidity + '%');
-                    $(current_UV).html('UV Index: '+ data.current.uvi);
+                    $(current_UV).html('UV Index: ' + data.current.uvi);
                     console.log("weatherdata");
                     console.log(data);
 
@@ -67,7 +60,7 @@ function getWeather(lat, lon) {
                     })
                 })
             } else alert("Error getting weather data");
-    })
+        })
 };
 
 //Search button handler.
@@ -80,10 +73,40 @@ $("#btnSearch").on("click", function () {
     getGeoCode(city);
 });
 
-function saveLocal() {
-    //funtion data here
-}
+// save to local storage function 
+// Checks if the city exists in the localhistory already. Adds if not and then reloads the front-end data. 
+function saveLocal(city) {
+    const loadHistory = localStorage.getItem("Weather-Dashboard");
+    // check to see if a localstorage item exists for 'weather-dashboard'
+    if (loadHistory != null) {
+        let searchHistory = JSON.parse(loadHistory);
+        // If 'city' doesn't exist in the localStorage, add it. 
+        if (!searchHistory.includes(city)) {
+            searchHistory.push(city);
+            localStorage.setItem("Weather-Dashboard", JSON.stringify(searchHistory));
+        };
+    } else {
+        let searchHistory = [city];
+        localStorage.setItem("Weather-Dashboard", JSON.stringify(searchHistory));
+    };
+    loadLocal(); // reload the search history in the front-end
+};
 
+// load from local storage and update front-end
 function loadLocal() {
- //funtion data here
-}
+    const loadHistory = localStorage.getItem("Weather-Dashboard");
+    if (loadHistory != null) {
+        let searchHistory = JSON.parse(loadHistory);
+        $("#searchHistory").html(""); //Blank the existing history
+        $.each(searchHistory, function (index, value) {
+            $("#searchHistory").append("<button class='btn btn-secondary col-12'>" + value + "</button>");
+        });
+        // Load the button click events for the generated buttons
+        $("#searchHistory button").click(function (event) {
+            const city = $(this).text();
+            getGeoCode(city);
+        });
+    };
+};
+// run the localstorage load on page-load 
+loadLocal();
